db6335b53f7e35b9997c9a5eabae464426d03407
Thejas Thomas <thejasthomas001@gmail.com>
2026-03-16 12:51:07 +0530

    after set up ci/cd

--- src/interfaceAdapters/database/mongoDb/models/admin_model.ts
+++ src/interfaceAdapters/database/mongoDb/models/admin_model.ts
@@ -5,5 +5,5 @@ import { adminSchema } from "../schemas/admin_schema";
 export interface IAdminModel extends IAdminEntity,Document {
     _id:ObjectId;
 }
-
+ 
 export const AdminModel =model<IAdminModel>('Admin',adminSchema)
